a1e243d0d8eba157e4ac671d5c4f0534ee4c8297
Sarin Iamsangtham <Iamsangsam@gmail.com>
2018-11-14 09:56:53 -0500

    Login/out
    Hopefully implemented a proper login/out system. Need to clear out some session stuff probably, but otherwise it works somewhat. Logout button still not functional

--- web.py
+++ web.py
@@ -6,12 +6,13 @@
 
 from flask import Flask, render_template, flash, redirect, url_for, session, logging,request
 from flask_wtf import FlaskForm
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from werkzeug import secure_filename
 
+
 #Okay, maybe import this instead for SQL stuff
 from flaskext.mysql import MySQL
-
 
 from passlib.hash import sha256_crypt
 from spellchecker import SpellChecker
@@ -20,10 +21,13 @@ from spellchecker import SpellChecker
 #import mysql's connector to get that functionality
 import mysql.connector
 
-
-
-
+#Setup flask
 app = Flask(__name__)
+
+
+#login manager to help with sessions
+login = LoginManager(app)
+
 
 
 mysql = MySQL()
@@ -39,9 +43,46 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['SECRET_KEY'] = 'BolognaBoys'
 
 
+#Initialize the mysql and login with the app
 mysql.init_app(app)
+login.init_app(app)
 
+#connect app to database
 conn = mysql.connect()
+
+#Make a user class of some sort
+class User(UserMixin):
+    def __init__(self, un, p, i):
+        self.username = un;
+        self.passhash = p;
+        self.id = i
+        self.active = True
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+#Set user_loader callback
+@login.user_loader
+def load_user(id):
+    cursor = conn.cursor()
+    ##get whatever's associated with the user
+    cursor.execute('''SELECT * FROM userpass WHERE id = %s''', id)
+            
+            
+    ##Check each item that matches (Probably just one now but just to be safe)
+    storeduser = cursor.fetchone()
+    return User(storeduser[1], storeduser[2], storeduser[0])
+
+
 
 
 @app.route("/",methods=['GET','POST'])
@@ -53,34 +94,53 @@ def login():
 
     if(request.method=='POST'):
         username = form.username.data
-        password = form.password.data
+        
 
 
         try:
             ##get the hashed password corresponding to the username entered
-            cursor.execute('''SELECT pass FROM userpass WHERE user = %s''', username)
-            
-            row = cursor.fetchone()
-            ##Check each item that matches (Probably just one now but just to be safe)
+            cursor.execute('''SELECT * FROM userpass WHERE user = %s''', username)
+                
+                
+                ##Check each item that matches (Probably just one now but just to be safe)
+            storeduser = cursor.fetchone()
+
+                ##If the password is verified to the stored hash, "log in"
+            if (sha256_crypt.verify(form.password.data, storeduser[2])):
 
 
-            ##If the password is verified to the stored hash, "log in"
-            if ((sha256_crypt.verify(password, row[0]))):
+                    #Make a new user object to log in for now?
+                user = User(username, storeduser[2], storeduser[0])
+
+                login_user(user)
+                    
                 cursor.close()
+
+
+                    
+
+                    #I don't know, just make it so session is of the username
+                session['name'] = username
+                session['logged_in'] = True
+                    
+
+                app.logger.info('%s logged in successfully', username)
+
+                    
                 return  redirect(url_for('spellcheck'))
                 
-                
-            
-            
-            cursor.close()
-            return  render_template('login.html', form=form, message="Invalid Username or Password")
+                    
+            else:    
+                cursor.close()
+                abort(401)
+                return  render_template('login.html', form=form, message="Invalid Username or Password")
 
             
             
         #In the case of some error, just redirect back to the login page for now
         except:
             cursor.close()
-            return   render_template('login.html', form=form, message="Invalid Username or Password")
+            return   render_template('login.html', form=form, message="Unknown Error: please try again")
     
 
 
@@ -108,7 +168,6 @@ def register():
 
     cursor = conn.cursor()
     
-    #not very sure how to connect the user's input into the form
     form = RegisterForm(request.form)
 
     
@@ -141,6 +200,9 @@ def register():
 
 @app.route("/spellcheck", methods=['GET','POST'])
 def spellcheck():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    
     if (request.method == 'POST'):
         spell = SpellChecker()
 
@@ -175,11 +237,17 @@ def spellcheck():
                 
             return render_template('spellcheck.html', webresult=result)
         else:
-            return redirect(url_for('spellcheck'))
+            return render_template('spellcheck.html', webresult="No Typos here")
         
     return render_template('spellcheck.html', webresult="None So Far!")
 
+#Route for logouts
+@app.route("/logout")
+def logout():
 
+    logout_user()
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
